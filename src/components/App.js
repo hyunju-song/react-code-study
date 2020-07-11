af389e749d023c94eacb4c49c8c7d62a864495b9
hyunjusong <hun3780@gmail.com>
2020-07-11 18:00:01 +0900

    ing recast.ly

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Nav from './Nav';
 import VideoPlayer from './VideoPlayer';
 import VideoList from './VideoList';
+import { fakeData } from "./__test__/fakeData";
 
-const App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentVideo: fakeData[0],
+      data: fakeData,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.data}/>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App;
